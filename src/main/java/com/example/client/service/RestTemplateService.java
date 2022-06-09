e2e9fb2d053f147c882901cc04fb6d5c059ec63b
iniline <1004dlagywls@naver.com>
2022-06-09 16:51:47 +0900

    Client Server to Server 연결

---
+++ src/main/java/com/example/client/service/RestTemplateService.java
@@ -0,0 +1,35 @@
+package com.example.client.service;
+
+import com.example.client.dto.UserResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+@Service
+public class RestTemplateService {
+    // http://localhost/api/server/hello
+    // response
+    public UserResponse hello() {
+        URI uri = UriComponentsBuilder
+                .fromUriString("http://localhost:9090")
+                .path("/api/server/hello")
+                .queryParam("name","aaaa")
+                .queryParam("age",99)
+                .encode()
+                .build()
+                .toUri();
+
+        System.out.println(uri.toString());
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<UserResponse> result = restTemplate.getForEntity(uri, UserResponse.class);
+
+
+        System.out.println(result.getStatusCode());
+        System.out.println(result.getBody());
+
+        return result.getBody();
+    }
+}
